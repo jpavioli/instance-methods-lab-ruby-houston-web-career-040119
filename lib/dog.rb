3630f91792ffe56809a3c1a6f8da702c4467f979
Jusitn Avioli <justin.avioli@gmail.com>
2019-04-02 22:23:52 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog 
+  #add an instance method #bark that puts "Woof!"
+  def bark
+    puts "Woof!"
+  end
+  #add an instance method #sit that puts "The Dog is sitting"
+  def sit 
+    puts "The Dog is sitting"
+  end 
+end
