4434d3d8ae717ef70a9b32bf2595a582d607fe6e
Jusitn Avioli <justin.avioli@gmail.com>
2019-04-02 22:23:45 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,10 @@
+class Person
+  #Add an instance method #talk that will puts "Hello World!"
+  def talk
+    puts "Hello World!"
+  end
+  #Add an instance method #walk that will puts "The Person is walking"
+  def walk 
+    puts "The Person is walking"
+  end
+end
